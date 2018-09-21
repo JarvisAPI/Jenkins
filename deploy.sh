589dd41d74aee39d13701c30c4a6c69e83310f9a
William Ou <ouhaoqian@hotmail.com>
2018-09-21 02:24:56 +0000

    - Modified port

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,11 @@ port=8080
 docker build -t jenkins .
 docker rm gallent_jones -f
 # docker run --name gallent_jones -d -p 8080:8080 -p 50001:50000 -v jenkins_home:/var/jenkins_home jenkins
-docker run --name gallent_jones -d -p $port:8080 -p 50001:50000 \
+docker run --name gallent_jones \
+       --network host \
+       -d \
+       -p $port:8080 \
+       -p 50001:50000 \
        -v jenkins_home:/var/jenkins_home \
        -v /var/run/docker.sock:/var/run/docker.sock \
        jenkins \
